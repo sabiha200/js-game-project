16d4202ed887bc81ab8b9f2fa5abc77b0f10cefe
Sabiha Jahan Prity <sabihajahanprity2004@gmail.com>
2025-05-03 12:04:33 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const startGame = document.querySelector(".startGame");
+const startbtn = document.querySelector(".startbtn");
+
+const gameBox = document.querySelector(".game");
+const player = document.querySelector(".player");
+const inputValue = document.querySelector(".inputValue");
+
+const submit = document.querySelector(".submit");
+
+startbtn.addEventListener("click",()=>{
+    console.log("click");
+    
+})
